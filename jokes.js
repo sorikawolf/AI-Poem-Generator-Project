25593838bab5e9f7b5a7312e152774cca1805179
sorikawolf <jennsorikawolf@gmail.com>
2026-02-22 22:45:07 -0500

    Added basic JavaScript to form
    added typewriter plugin

--- jokes.js
+++ jokes.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: ["Generating poem..."],
+    autoStart: true,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator");
+poemFormElement.addEventListener("submit", generatePoem);
